f6c180bba34b52c48db700023f8014fbe885c255
JohnsonNYC <jkow95@gmail.com>
2020-03-03 23:16:40 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,16 +1,21 @@
+require "pry"
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.songs.first.genre #How is this called "genre" and not "genres". Help me understand chainning attributes please. How does pry work in this file?? Is it becuase we are using attributes defined in songs model attribute
+     #To run pry, you must migrate file and run learn. Do CRUD methods belong to class file or a run file?
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
+    self.genres.count
     #return the number of genres associated with the artist
   end
 end
